c9340fa6e44a1d5987f5be870943ad4cd3548d15
Tal Shachar <tal.shachar@auctionata.com>
2018-01-19 12:39:37 +0100

    wip

--- src/test/java/enfore/appium/setup/Definitions.java
+++ src/test/java/enfore/appium/setup/Definitions.java
@@ -14,31 +14,12 @@ import cucumber.api.java.en.And;
 public class Definitions extends AppiumSetup{
 	@Given("^I am on login screen")
 	public void i_am_on_login_screen() throws Throwable {
-		setupCaps();
 		
 	}
 
 	@When("^I login with a valid user$")
 	public void i_login_with_a_valid_user() throws Throwable {
-		loginPage.loginWithValidUser();
-	}
-
-	@When("^some other action$")
-	public void some_other_action() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@When("^yet another action$")
-	public void yet_another_action() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Then("^I validate the outcomes$")
-	public void i_validate_the_outcomes() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
 	}
 
 	@Then("^check more outcomes$")
